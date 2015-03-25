53b105ae4f3da02877faab6327e2837183bc37d3
Emily Sas <emily.bronwen.sas@gmail.com>
2015-03-25 15:41:32 +0000

    adding first test - fizz if factor of 3

---
+++ fizzbuzz_tests/fizzbuzztest.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace fizzbuzz_tests
+{
+
+	[TestFixture]
+	public class FizzBuzz
+	{
+
+		[Test]
+		public void FizzIfFactorOf3()
+		{
+			Game game = new Game();
+			Assert.AreEqual ("fizz", game.FizzBuzz (3));
+		}
+			
+	}
+}
+
